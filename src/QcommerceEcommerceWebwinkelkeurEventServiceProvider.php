c917498e6c3de866dcc83537cfd3aa93cc090338
Robinvm <Robinvm@users.noreply.github.com>
2022-03-02 08:47:03 +0000

    Fix styling

--- src/QcommerceEcommerceWebwinkelkeurEventServiceProvider.php
+++ src/QcommerceEcommerceWebwinkelkeurEventServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace Qubiqx\QcommerceEcommerceWebwinkelkeur;
 
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Qubiqx\QcommerceEcommerceCore\Events\Orders\OrderMarkedAsPaidEvent;
 use Qubiqx\QcommerceEcommerceWebwinkelkeur\Listeners\SendReviewEmailListener;
-use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class QcommerceEcommerceWebwinkelkeurEventServiceProvider extends ServiceProvider
 {
